19af6ee216033a526e283c53d9108256b4220b05
iamhabbeboy <iamhabbeboy@gmail.com>
2021-01-29 10:24:46 +0100

    slight UI changes + add synthesis

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import { useSpeechSynthesis } from "react-speech-kit";
 
-function App() {
+const App = () => {
+  const [value, setValue] = useState(
+    "In the beginning the lord created the heaven and the earth."
+  );
+  const { speak } = useSpeechSynthesis();
+  // setValue("In the beginning the lord created the heaven and the earth.");
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="container">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt,
+            excepturi ut deleniti corrupti, quaerat, voluptate blanditiis est
+            repellendus omnis laudantium eius sit perferendis explicabo nulla
+            quibusdam quos soluta dolorum quasi.
+          </p>
+          <p className="verses">Genesis 10:5</p>
+          <label>
+            <input type="checkbox" />
+            Read bible verse by speaking with synthesis{" "}
+          </label>
+          <br />
+          <input type="text" placeholde="set interval" />
+          <br />
+          <button className="App-link" onClick={() => speak({ text: value })}>
+            Shoot
+          </button>
+        </div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
